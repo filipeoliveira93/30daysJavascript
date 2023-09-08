d8d00957e865b29677000358cf5ed166620c38bf
Filipe Oliveira <61127389+filipeoliveira93@users.noreply.github.com>
2023-09-07 21:55:20 -0300

    add `nodemon` to server nodejs

---
+++ day-01/variable.js
@@ -0,0 +1,13 @@
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+let strings = 'lorens ipsuns'
+let boolean = true
+const undefinedVar = undefined;
+let nll = null
+let number = 245
+
+console.log(`strings = ${strings} type: ${typeof strings}`);
+console.log(`boolean = ${boolean} type: ${typeof boolean}`);
+console.log(`undefinedVar = ${undefinedVar} type ${typeof undefinedVar}`);
+console.log(`nll = ${null} = type ${typeof nll}`);
+console.log(`number = ${typeof number}`);
